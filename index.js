9c616883fa912cc6eb535887d9a95a15eae3e1ff
travis-john <travis.wl.john@gmail.com>
2020-01-21 10:55:42 -0500

    adding global variables / adding inquirer.js as module

---
+++ index.js
@@ -0,0 +1,11 @@
+//global requirements
+const fs = require('fs'),
+      util = require('util'),
+      path = require('path'),
+      open = require('open'),
+      const Employee = require('./assets/js/Employee'),
+      const Engineer = require('./assets/js/Engineer'),
+      const Intern = require('./assets/js/Intern'),
+      const Manager = require('./assets/js/Manager'),
+      readFile = util.promisifify(fs.readFile),
+      writeFile = util.promisify(fs.writeFile);
