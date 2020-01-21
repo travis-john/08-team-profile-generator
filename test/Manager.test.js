ce6b2c86a049f74d108bf727bf81010970a3ba01
travis-john <travis.wl.john@gmail.com>
2020-01-21 16:36:09 -0500

    changing Manager.test.js so that tests match Manager.js

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -4,7 +4,7 @@ const Employee = require("../assets/js/Employee");
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.officeNumber).toBe(testValue);
+  expect(e.phone).toBe(testValue);
 });
 
 test("getRole() should return \"Manager\"", () => {
@@ -16,5 +16,5 @@ test("getRole() should return \"Manager\"", () => {
 test("Can get office number via getOffice()", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
+  expect(e.getNumber()).toBe(testValue);
 });
